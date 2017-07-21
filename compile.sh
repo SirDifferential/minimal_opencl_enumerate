ed023b4a4f35b38d15e2b738769e2b2e186dde5b
Jesse Kaukonen <jesse.kaukonen@gmail.com>
2017-07-21 16:47:40 +0300

    nvidia specific include

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo g++ -std=c++11 -Wall -I/usr/include -L/usr/lib/x86_64-linux-gnu -lOpenCL main.cpp -o cltest
-g++ -std=c++11 -Wall -I/usr/include -L/usr/lib/x86_64-linux-gnu -lOpenCL main.cpp -o cltest
+echo g++ -std=c++11 -Wall -I/usr/include -I/usr/local/cuda/include -L/usr/lib/x86_64-linux-gnu -lOpenCL main.cpp -o cltest
+g++ -std=c++11 -Wall -I/usr/include -I/usr/local/cuda/include -L/usr/lib/x86_64-linux-gnu -lOpenCL main.cpp -o cltest
